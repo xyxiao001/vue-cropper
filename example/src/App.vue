7817ca00f80beb97a929d2cb3dfae4064d3514de
xyxiao <463618622@qq.com>
2017-05-25 18:11:08 +0800

    添加图片布局

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,6 +1,8 @@
 <template>
 	<div class="content">
-		<vueCropper></vueCropper>
+		<h1>vue-cropper</h1>
+		<p>一个基于vue的图片裁剪插件</p>
+		<vueCropper ref="cropper" :img="option.img2"></vueCropper>
 	</div>
 </template>
 
@@ -9,13 +11,24 @@ import vueCropper from './vue-cropper'
 export default {
   data: function () {
     return {
+			option: {
+				img1: 'https://fengyuanchen.github.io/cropper/images/picture.jpg',
+				img2: 'http://ofyaji162.bkt.clouddn.com/touxiang.jpg',
+				img3: 'https://o90cnn3g2.qnssl.com/0C3ABE8D05322EAC3120DDB11F9D1F72.png',
+			}
     }
   },
 	components: {
 		vueCropper
+	},
+	mounted () {
 	}
 }
 </script>
 
 <style scoped>
+	.content {
+		margin: auto;
+		max-width: 1200px;
+	}
 </style>
--- example/src/vue-cropper/vue-cropper.vue
+++ example/src/vue-cropper/vue-cropper.vue
@@ -1,6 +1,21 @@
 <template>
-	<div class="vue-cropper">
-		vue-cropper
+	<div class="vue-cropper" ref="cropper">
+		<div class="cropper-box">
+			<div class="cropper-box-canvas"
+			 	v-show="!loading"
+				:style="{
+					'width': trueWidth + 'px',
+					'height': trueHeight + 'px',
+					'transform': 'scale(' + scale + ',' + scale + ') ' + 'translate3d('+ x + 'px,' + y + 'px,' + '0)'
+					}">
+				<img
+					:src="img"
+					alt="cropper-img"
+					ref="cropperImg"
+					>
+			</div>
+		</div>
+		<div class="cropper-move"></div>
 	</div>
 </template>
 
@@ -8,10 +23,97 @@
 export default {
   data: function () {
     return {
+			scale: 1,
+			x: 0,
+			y: 0,
+			loading: true,
+			trueWidth: 0,
+			trueHeight: 0
     }
-  }
+  },
+	props: {
+		img: {
+			type: String,
+			default: ''
+		}
+	},
+	watch: {
+		// 如果图片改变， 重新布局
+		img () {
+			this.$refs.cropperImg.onload = () => {
+        // 图片加载成功后布局
+				this.reload()
+      }
+		}
+	},
+	methods: {
+		// reload 图片布局函数
+		reload () {
+			// 得到外层容器的宽度高度
+			let w =  window.getComputedStyle(this.$refs.cropper).width.replace('px', '') - 4
+			let h =  window.getComputedStyle(this.$refs.cropper).height.replace('px', '') - 4
+
+			// 存入图片真实高度
+			this.trueWidth = this.$refs.cropperImg.width
+			this.trueHeight = this.$refs.cropperImg.height
+
+			if (this.trueWidth > w) {
+				// 如果图片宽度大于容器宽度
+				this.scale = w / this.trueWidth
+			}
+
+			if (this.trueHeight * this.scale > h) {
+				this.scale = h / this.trueHeight
+			}
+
+			this.$nextTick(() => {
+				this.x = -(this.trueWidth - this.trueWidth * this.scale) / 2 + (w - this.trueWidth * this.scale) / 2
+				this.y = -(this.trueHeight - this.trueHeight * this.scale) / 2 + (h - this.trueHeight * this.scale) / 2
+				this.loading = false
+				console.log('reload')
+			})
+		},
+		// 重置函数， 恢复组件置初始状态
+		refresh () {
+			console.log('refresh')
+		}
+	},
+	mounted () {
+		this.$refs.cropperImg.onload = () => {
+			// 图片加载成功后布局
+			this.reload()
+		}
+	}
 }
 </script>
 
 <style scoped>
+	.vue-cropper {
+		position: relative;
+		width: 100%;
+		height: 500px;
+		padding: 2px;
+		box-sizing: border-box;
+		user-select: none;
+		direction: ltr;
+		touch-action: none;
+		overflow: hidden;
+  	background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAAA3NCSVQICAjb4U/gAAAABlBMVEXMzMz////TjRV2AAAACXBIWXMAAArrAAAK6wGCiw1aAAAAHHRFWHRTb2Z0d2FyZQBBZG9iZSBGaXJld29ya3MgQ1M26LyyjAAAABFJREFUCJlj+M/AgBVhF/0PAH6/D/HkDxOGAAAAAElFTkSuQmCC');
+	}
+
+	.cropper-box, .cropper-box-canvas, .cropper-move {
+		position: absolute;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+	}
+
+	.cropper-box-canvas img {
+		position: relative;
+	}
+
+	.cropper-move {
+		cursor: move;
+	}
 </style>
