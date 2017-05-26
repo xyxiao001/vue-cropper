f6a1dac339b4ce5f90e918d2f2dac73951a9f732
xyxiao <463618622@qq.com>
2017-05-26 11:53:38 +0800

    添加图片缩放

--- example/src/App.vue
+++ example/src/App.vue
@@ -2,7 +2,8 @@
 	<div class="content">
 		<h1>vue-cropper</h1>
 		<p>一个基于vue的图片裁剪插件</p>
-		<vueCropper ref="cropper" :img="option.img3"></vueCropper>
+		<vueCropper ref="cropper" :img="option[num].img"></vueCropper>
+		<button @click="changeImg">改变图片</button>
 	</div>
 </template>
 
@@ -11,17 +12,42 @@ import vueCropper from './vue-cropper'
 export default {
   data: function () {
     return {
-			option: {
-				img1: 'https://fengyuanchen.github.io/cropper/images/picture.jpg',
-				img2: 'http://ofyaji162.bkt.clouddn.com/touxiang.jpg',
-				img3: 'https://o90cnn3g2.qnssl.com/0C3ABE8D05322EAC3120DDB11F9D1F72.png',
-			}
+			option: [
+				{
+					img: 'https://fengyuanchen.github.io/cropper/images/picture.jpg'
+				},
+				{
+					img: 'http://ofyaji162.bkt.clouddn.com/touxiang.jpg'
+				},
+				{
+					img: 'https://o90cnn3g2.qnssl.com/0C3ABE8D05322EAC3120DDB11F9D1F72.png'
+				},
+				{
+					img: 'http://ofyaji162.bkt.clouddn.com/bg1.jpg',
+				},
+				{
+					img: 'http://ofyaji162.bkt.clouddn.com/bg2.jpg',
+				},
+				{
+					img: 'http://ofyaji162.bkt.clouddn.com/can.jpg'
+				},
+				{
+					img: 'http://ofyaji162.bkt.clouddn.com/nightcat.jpg'
+				}
+			],
+			num: 0,
     }
   },
+	methods: {
+		changeImg () {
+			this.num = ~~(Math.random() * this.option.length)
+		}
+	},
 	components: {
 		vueCropper
 	},
 	mounted () {
+		this.num = ~~(Math.random() * this.option.length)
 	}
 }
 </script>
--- example/src/vue-cropper/vue-cropper.vue
+++ example/src/vue-cropper/vue-cropper.vue
@@ -6,7 +6,7 @@
 				:style="{
 					'width': trueWidth + 'px',
 					'height': trueHeight + 'px',
-					'transform': 'scale(' + scale + ',' + scale + ') ' + 'translate3d('+ x + 'px,' + y + 'px,' + '0)'
+					'transform': 'scale(' + scale + ',' + scale + ') ' + 'translate3d('+ x / scale + 'px,' + y / scale + 'px,' + '0)'
 					}">
 				<img
 					:src="img"
@@ -15,7 +15,15 @@
 					>
 			</div>
 		</div>
-		<div class="cropper-move"></div>
+		<div
+			class="cropper-drag-box"
+		  :class="{'cropper-move': move}"
+			@mousedown="startMove"
+      @touchstart="startMove"
+			@mouseover="scaleImg"
+			@mouseout="cancleScale"
+			>
+		</div>
 	</div>
 </template>
 
@@ -23,12 +31,26 @@
 export default {
   data: function () {
     return {
+			// 图片缩放比例
 			scale: 1,
+			// 图片偏移x轴
 			x: 0,
+			// 图片偏移y轴
 			y: 0,
+			// 图片加载
 			loading: true,
+			// 图片真实宽度
 			trueWidth: 0,
-			trueHeight: 0
+			// 图片真实高度
+			trueHeight: 0,
+			// 是否可以拖动图片
+			move: true,
+			// 移动的x
+			moveX: 0,
+			// 移动的y
+			moveY: 0,
+			// 是否开启滚轮放大缩小
+			canScale: true
     }
   },
 	props: {
@@ -40,6 +62,8 @@ export default {
 	watch: {
 		// 如果图片改变， 重新布局
 		img () {
+			this.loading = true
+			this.scale = 1
 			this.$refs.cropperImg.onload = () => {
         // 图片加载成功后布局
 				this.reload()
@@ -47,6 +71,51 @@ export default {
 		}
 	},
 	methods: {
+		// 当按下鼠标键
+		startMove (e) {
+			// 如果move 为true 表示当前可以拖动
+			if (this.move) {
+				// 开始移动
+				window.addEventListener('mousemove', this.moveImg)
+      	window.addEventListener('touchmove', this.moveImg)
+      	window.addEventListener('mouseup', this.leaveImg)
+      	window.addEventListener('touchend', this.leaveImg)
+				this.moveX = (event.clientX ? event.clientX : event.touches[0].clientX) - this.x
+	      this.moveY = (event.clientY ? event.clientY : event.touches[0].clientY) - this.y
+			}
+		},
+		// 移动图片
+		moveImg (e) {
+			var nowX = event.clientX ? event.clientX : event.touches[0].clientX
+      var nowY = event.clientY ? event.clientY : event.touches[0].clientY
+			this.$nextTick(() => {
+				this.x = ~~(nowX - this.moveX)
+				this.y = ~~(nowY - this.moveY)
+			})
+		},
+		leaveImg (e) {
+			window.removeEventListener('mousemove', this.moveImg)
+      window.removeEventListener('touchmove', this.moveImg)
+      window.removeEventListener('mouseup', this.leaveImg)
+      window.removeEventListener('touchend', this.leaveImg)
+		},
+		// 缩放图片
+		scaleImg () {
+			if (this.canScale) {
+				window.addEventListener('mousewheel', this.changeSize)
+			}
+		},
+		cancleScale () {
+			if (this.canScale) {
+				window.removeEventListener('mousewheel', this.changeSize)
+			}
+		},
+		// 改变大小函数
+		changeSize (e) {
+			var change = event.deltaY
+			change < 0 ? this.scale += 0.1 : this.scale > 0.1 ? this.scale -= 0.1 : this.scale
+			event.preventDefault()
+		},
 		// reload 图片布局函数
 		reload () {
 			// 得到外层容器的宽度高度
@@ -68,8 +137,8 @@ export default {
 
 			this.$nextTick(() => {
 				// this.x = this.trueWidth * (this.scale - 1)
-				this.x = (-(this.trueWidth - this.trueWidth * this.scale) / 2 + (w - this.trueWidth * this.scale) / 2) / this.scale
-				this.y = (-(this.trueHeight - this.trueHeight * this.scale) / 2 + (h - this.trueHeight * this.scale) / 2) / this.scale
+				this.x = -(this.trueWidth - this.trueWidth * this.scale) / 2 + (w - this.trueWidth * this.scale) / 2
+				this.y = -(this.trueHeight - this.trueHeight * this.scale) / 2 + (h - this.trueHeight * this.scale) / 2
 				this.loading = false
 				console.log('reload')
 			})
@@ -97,11 +166,11 @@ export default {
 		user-select: none;
 		direction: ltr;
 		touch-action: none;
-		/*overflow: hidden;*/
+		overflow: hidden;
   	background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAAA3NCSVQICAjb4U/gAAAABlBMVEXMzMz////TjRV2AAAACXBIWXMAAArrAAAK6wGCiw1aAAAAHHRFWHRTb2Z0d2FyZQBBZG9iZSBGaXJld29ya3MgQ1M26LyyjAAAABFJREFUCJlj+M/AgBVhF/0PAH6/D/HkDxOGAAAAAElFTkSuQmCC');
 	}
 
-	.cropper-box, .cropper-box-canvas, .cropper-move {
+	.cropper-box, .cropper-box-canvas, .cropper-drag-box {
 		position: absolute;
 		top: 0;
 		right: 0;
@@ -111,6 +180,10 @@ export default {
 
 	.cropper-box-canvas img {
 		position: relative;
+	}
+
+	.crapper-drag-box {
+		z-index: 2;
 	}
 
 	.cropper-move {
