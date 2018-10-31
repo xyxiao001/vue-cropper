32515b607d8592e136a884491fc26a64ea77f307
hz <hz932@qq.com>
2018-10-31 18:30:30 +0800

    Update vue-cropper.vue
    空白字符

--- src/vue-cropper.vue
+++ src/vue-cropper.vue
@@ -253,8 +253,8 @@ export default {
     },
 
     isIE () {
-      var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
-      var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1; //判断是否IE浏览器
+      var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串  
+      var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1; //判断是否IE浏览器  
       return isIE
     },
   },
