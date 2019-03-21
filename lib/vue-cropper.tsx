1351e5fd7b53f5bdcae9045bd2def68ea3944e51
xyxiao001 <463618622@qq.com>
2019-03-21 10:44:15 +0800

    feat: add img load

--- lib/vue-cropper.tsx
+++ lib/vue-cropper.tsx
@@ -42,17 +42,27 @@ export default class VueCropper extends Vue {
 
   // 检查图片
   checkedImg  = async (url: string) => {
+    let img:HTMLImageElement
     try {
-      await this.loadImg(url)
+      img = await this.loadImg(url)
       this.imgLoad('success')
     } catch (error) {
       this.imgLoad(error)
+      console.log('img load error')
+      return false      
     }
-    console.log(1)
+    console.log('img load success')
+    // 图片加载成功之后的操作
+    // CanvasRenderingContext2D
+    const ctx:any = this.$refs.canvas.getContext('2d')
+    this.$refs.canvas.width = img.width
+    this.$refs.canvas.height = img.height
+    ctx.drawImage(img, 0, 0, img.width, img.height)
+    return true
   }
 
   // 加载图片
-  loadImg = async (url: string) => {
+  loadImg = async (url: string):Promise<HTMLImageElement> => {
     return new Promise((resolve, reject) => {
       const img = new Image()
       img.onload = () => resolve(img)
