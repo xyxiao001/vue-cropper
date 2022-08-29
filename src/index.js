e512016a0423ce793c411d1ee1672d0cd4ac7c79
徐景建 <xjj314@163.com>
2022-08-29 22:34:08 +0800

    修改版本

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 export { VueCropper }
 
 export default {
-  version: '0.5.6',
+  version: '0.5.7',
   install,
   VueCropper,
   vueCropper: VueCropper
